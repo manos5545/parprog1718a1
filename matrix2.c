2bca72907f0ec8bb79e6509ac6eb59588e7b0aff
manos5545 <manos5545@yahoo.gr>
2018-03-12 03:59:17 +0200

    Update matrix2.c

--- matrix2.c
+++ matrix2.c
@@ -1,11 +1,13 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
 
-// compile like:  gcc -Wall -O2 -DNROWS=10000 matrix2.c -o matrix2
+// compile like:  gcc -Wall -O2 -DNROWS=10000 matrix1.c -o matrix1
 
 
 #define NCOLS 100
+#define NROWS 100000
 
 void get_walltime(double *wct) {
   struct timeval tp;
@@ -25,30 +27,42 @@ double ts,te;
     exit(1);
   }
 
-  // warmup
-
-  // ...your code here...
-
+  
+  	int i,j =0;
+  	
+	for (i=0; i<NROWS*NCOLS; i++) {
+		table[i] = 1.0;
+	}//We assign 1.0 beacuse we have double values and value of 1 to check accesses
+	double sum = 0.0;
+	
+	
   // get starting time (double, seconds) 
   get_walltime(&ts);
   
   // workload
-
-  // ...your code here...
+	for (i=0; i<NCOLS; i++) {
+		for (j=0; j<NROWS; j++) {
+			sum += table[i*NROWS+j];
+		}
+	}
 
   // get ending time
   get_walltime(&te);
 
-  // check results
-  
-  // ...your code here...
+  double time_elapsed = te - ts;
 
-  // print time elapsed and/or Maccesses/sec
+  printf ("Time elapsed is: %fsec\n", time_elapsed);
   
-  // ...your code here...  
+
+  // check results
+  printf("sum = %f\n", sum);
+  
+  double m_acc = ((double)NROWS*NCOLS)/((te-ts)*1e6);
+  
+  printf("average array element Maccesses/sec = %f\n", m_acc);
+  
   
   free(table);
 
   return 0;
 }
-
